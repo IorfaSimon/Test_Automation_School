264f1dfa257de71ebcad6a6a1ed1d609718cd772
IorfaSimon <simoniorfa@gmail.com>
2023-08-31 00:33:27 +0100

    update Module_4

---
+++ Module_4/Tasks/Task10.java
@@ -0,0 +1,17 @@
+package com.simon;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Task10 task10Var = new Task10();
+        task10Var.printAcceptanceMessage();
+        task10Var.printRejectionMessage();
+
+    }
+
+    public void printAcceptanceMessage() {
+        System.out.println("welcome to Testify");
+    }
+    public void printRejectionMessage(){
+        System.out.println("You are not accepted");
+    }
+}
