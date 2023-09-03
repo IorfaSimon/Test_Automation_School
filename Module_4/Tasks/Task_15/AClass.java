cfc31274d112a236cabde4ad265bf8579ade6595
IorfaSimon <simoniorfa@gmail.com>
2023-09-03 22:38:41 +0100

    update module_4

---
+++ Module_4/Tasks/Task_15/AClass.java
@@ -0,0 +1,15 @@
+package com.Task_15;
+
+public class AClass {
+    public void makesFun(){
+
+    }
+    public void darkSkinned(){
+
+    }
+    public void likesReading(){
+
+    }
+
+
+}
---
+++ Module_4/Tasks/Task_15/BClass.java
@@ -0,0 +1,14 @@
+package com.Task_15;
+
+public class BClass extends AClass{
+    public static void main(String[] args) {
+        AClass aClass = new AClass();
+
+        aClass.makesFun();
+        System.out.println("He makes fun");
+
+        aClass.darkSkinned();
+        System.out.println("He is darkskinned");
+
+    }
+}
